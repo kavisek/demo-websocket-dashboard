3bd75d5d314cb8f3eb13874e1f4b736f00ae4b72
kavisek <kavi.skhon@gmail.com>
2022-04-29 20:19:32 +0000

    feat: testing cont in book object

--- containers/frontend/src/index.js
+++ containers/frontend/src/index.js
@@ -34,27 +34,22 @@ function Booklist() {
     return (
         <section className='booklist'>
             <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
         </section>
     );
 }
 
+const author = "Sample Author"
 const Book = () => {
+    const title = 'Sample Book'
     return (
         <article>
-            <Image></Image>
-            <Title />
-            <Author />
+            < img src="https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
+            <h1>{title}</h1>
+            <h4>{author.toUpperCase()}</h4>
+            {/* <p>{let x = 6 }</p> */}
+            <p>{6+6}</p>
         </article >
     );
 }
-
-const Image = () => < img src="https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
-const Title = () => <h1> Sample Book </h1>
-const Author = () => <h4 style={{color:'#617d98', fonSize: '0.75rem', marginTop: '0.25rem'}} > Sample Author </h4>
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
