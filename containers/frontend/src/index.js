55643b1bcb173ab6aae68eb7cd9000aeece33f0c
kavisek <kavi.skhon@gmail.com>
2022-04-29 21:19:14 +0000

    use id instaed of index

--- containers/frontend/src/index.js
+++ containers/frontend/src/index.js
@@ -33,11 +33,13 @@ import './index.css'
 //Setup Vars
 const books = [
     {
+        id: 1,
         img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
         title: 'Sample Book',
         author: "Sample Author",
     },
     {
+        id: 2,
         img: "https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
         title: 'Sample Book',
         author: "Sample Author",
@@ -49,7 +51,7 @@ function Booklist() {
     return (
         <section className='booklist'>
             {books.map((book, index) => {
-                return <Book key={index} book={book}></Book>
+                return <Book key={book.id} book={book}></Book>
             })}
         </section>
     );
