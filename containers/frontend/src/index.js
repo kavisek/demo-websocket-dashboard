cad6533ab35635f69db055095a6c878de00aea07
kavisek <kavi.skhon@gmail.com>
2022-04-29 18:24:48 +0000

    feat: book list template 3 with images

--- containers/frontend/src/index.js
+++ containers/frontend/src/index.js
@@ -42,10 +42,14 @@ const Book = () => {
     return (
         <article>
             <Image></Image>
+            <Title />
+            <Author />
         </article >
     );
 }
 
 const Image = () => < img src="https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
+const Title = () => <h1> Sample Book </h1>
+const Author = () => <h5> Sample Author </h5>
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
