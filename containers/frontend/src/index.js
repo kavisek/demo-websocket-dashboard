bfdc5ac9b9a565bcbc01182207ec65c41fe15bcc
kavisek <kavi.skhon@gmail.com>
2022-04-29 20:10:37 +0000

    feat: customr styling

--- containers/frontend/src/index.js
+++ containers/frontend/src/index.js
@@ -55,6 +55,6 @@ const Book = () => {
 
 const Image = () => < img src="https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
 const Title = () => <h1> Sample Book </h1>
-const Author = () => <h5> Sample Author </h5>
+const Author = () => <h4 style={{color:'#617d98', fonSize: '0.75rem', marginTop: '0.25rem'}} > Sample Author </h4>
 
 ReactDom.render(<Booklist />, document.getElementById('root'));
